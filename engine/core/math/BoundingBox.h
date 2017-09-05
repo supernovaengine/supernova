7d5abf656dbec0eb101764dc5b1f0ea0e41f5a54
eduardodoria <doriaeduardo@gmail.com>
2017-09-04 22:53:41 -0300

    Added Plane and BoundingBox classes

---
+++ engine/core/math/BoundingBox.h
@@ -0,0 +1,14 @@
+#ifndef BoundingBox_h
+#define BoundingBox_h
+
+namespace Supernova{
+    
+    class BoudingBox{
+        Vector3 normal;
+        float d;
+        
+    };
+    
+}
+
+#endif /* BoundingBox_h */
---
+++ engine/core/math/Plane.cpp
@@ -0,0 +1,3 @@
+#include "Plane.h"
+
+using namespace Supernova;
