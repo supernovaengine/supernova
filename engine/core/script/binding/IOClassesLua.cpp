17dbee020f3976895463b55527ea53c54d0f4a9e
eduardodoria <doriaeduardo@gmail.com>
2022-09-08 14:05:12 -0300

    Finished util classes Lua bindings

--- engine/core/script/binding/IOClassesLua.cpp
+++ engine/core/script/binding/IOClassesLua.cpp
@@ -10,8 +10,32 @@
 
 #include "sol.hpp"
 
+#include "io/Data.h"
+#include "io/File.h"
+#include "io/FileData.h"
+
 using namespace Supernova;
 
 void LuaBinding::registerIOClasses(lua_State *L){
     sol::state_view lua(L);
+/*
+    auto filedata = lua.new_usertype<FileData>("FileData",
+        sol::call_constructor, sol::default_constructor);
+
+    filedata["newFile"] = sol::overload(sol::resolve<FileData*(bool)>(FileData::newFile), sol::resolve<FileData*(const char *, bool)>(FileData::newFile));
+    filedata["newFile"] = &FileData::getBaseDir;
+    filedata["getFilePathExtension"] = &FileData::getFilePathExtension;
+    filedata["getSystemPath"] = &FileData::getSystemPath;
+    filedata["read8"] = &FileData::read8;
+    filedata["read16"] = &FileData::read16;
+    filedata["read32"] = &FileData::read32;
+    filedata["eof"] = &FileData::eof;
+    filedata["read"] = &FileData::read;
+    filedata["write"] = &FileData::write;
+    filedata["length"] = &FileData::length;
+    filedata["seek"] = &FileData::seek;
+    filedata["pos"] = &FileData::pos;
+    filedata["readString"] = &FileData::readString;
+    filedata["writeString"] = &FileData::writeString;
+    */
 }
--- engine/core/script/binding/UtilClassesLua.cpp
+++ engine/core/script/binding/UtilClassesLua.cpp
@@ -10,8 +10,35 @@
 
 #include "sol.hpp"
 
+#include "util/Angle.h"
+#include "util/Base64.h"
+#include "util/Color.h"
+
 using namespace Supernova;
 
 void LuaBinding::registerUtilClasses(lua_State *L){
     sol::state_view lua(L);
+
+    auto angle = lua.new_usertype<Angle>("Angle",
+        sol::no_constructor);
+
+    angle["radToDefault"] = Angle::radToDefault;
+    angle["degToDefault"] = Angle::degToDefault;
+    angle["defaultToRad"] = Angle::defaultToRad;
+    angle["defaultToDeg"] = Angle::defaultToDeg;
+    angle["radToDeg"] = Angle::radToDeg;
+    angle["degToRad"] = Angle::degToRad;
+
+    auto base64 = lua.new_usertype<Base64>("Base64",
+        sol::no_constructor);
+
+    base64["encode"] = Base64::encode;
+    base64["decode"] = Base64::decode;
+
+    auto color = lua.new_usertype<Color>("Color",
+        sol::no_constructor);
+
+    color["linearTosRGB"] = sol::overload(sol::resolve<Vector3(Vector3)>(Color::linearTosRGB), sol::resolve<Vector4(Vector4)>(Color::linearTosRGB));
+    color["sRGBToLinear"] = sol::overload(sol::resolve<Vector3(Vector3)>(Color::sRGBToLinear), sol::resolve<Vector4(Vector4)>(Color::sRGBToLinear));
+
 }
