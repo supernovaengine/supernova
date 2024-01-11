b409d0eb81bf6875ceff54a1d7b0b89c7376e466
eduardodoria <doriaeduardo@gmail.com>
2024-01-11 10:48:03 -0300

    Fixed broken texture y orientation in Emscripten

--- engine/libs/stb/stb_image.c
+++ engine/libs/stb/stb_image.c
@@ -1,4 +1,4 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STBI_FAILURE_USERMSG
-#define STBI_NO_THREAD_LOCALS
+#define STBI_NO_THREAD_LOCALS // without this broken texture y orientation in Emscripten
 #include "stb_image.h"
