928cc759262865e59918883121bfa0699d8be3c9
eduardodoria <doriaeduardo@gmail.com>
2025-01-08 11:02:14 -0300

    Updated tinyobjloader to release branch (with mods)

--- engine/libs/tinyobjloader/tiny_obj_loader.h
+++ engine/libs/tinyobjloader/tiny_obj_loader.h
@@ -2723,7 +2723,7 @@ bool LoadObj(attrib_t *attrib, std::vector<shape_t> *shapes,
 
       sw.vertex_id = vid;
 
-      while (!IS_NEW_LINE(token[0])) {
+      while (!IS_NEW_LINE(token[0]) && token[0] != '#') {
         real_t j, w;
         // joint_id should not be negative, weight may be negative
         // TODO(syoyo): # of elements check
@@ -2764,7 +2764,7 @@ bool LoadObj(attrib_t *attrib, std::vector<shape_t> *shapes,
 
       __line_t line;
 
-      while (!IS_NEW_LINE(token[0])) {
+      while (!IS_NEW_LINE(token[0]) && token[0] != '#') {
         vertex_index_t vi;
         if (!parseTriple(&token, static_cast<int>(v.size() / 3),
                          static_cast<int>(vn.size() / 3),
@@ -2795,7 +2795,7 @@ bool LoadObj(attrib_t *attrib, std::vector<shape_t> *shapes,
 
       __points_t pts;
 
-      while (!IS_NEW_LINE(token[0])) {
+      while (!IS_NEW_LINE(token[0]) && token[0] != '#') {
         vertex_index_t vi;
         if (!parseTriple(&token, static_cast<int>(v.size() / 3),
                          static_cast<int>(vn.size() / 3),
@@ -2830,7 +2830,7 @@ bool LoadObj(attrib_t *attrib, std::vector<shape_t> *shapes,
       face.smoothing_group_id = current_smoothing_id;
       face.vertex_indices.reserve(3);
 
-      while (!IS_NEW_LINE(token[0])) {
+      while (!IS_NEW_LINE(token[0]) && token[0] != '#') {
         vertex_index_t vi;
         if (!parseTriple(&token, static_cast<int>(v.size() / 3),
                          static_cast<int>(vn.size() / 3),
@@ -2966,7 +2966,7 @@ bool LoadObj(attrib_t *attrib, std::vector<shape_t> *shapes,
 
       std::vector<std::string> names;
 
-      while (!IS_NEW_LINE(token[0])) {
+      while (!IS_NEW_LINE(token[0]) && token[0] != '#') {
         std::string str = parseString(&token);
         names.push_back(str);
         token += strspn(token, " \t\r");  // skip tag
@@ -3260,7 +3260,7 @@ bool LoadObjWithCallback(std::istream &inStream, const callback_t &callback,
       token += strspn(token, " \t");
 
       indices.clear();
-      while (!IS_NEW_LINE(token[0])) {
+      while (!IS_NEW_LINE(token[0]) && token[0] != '#') {
         vertex_index_t vi = parseRawTriple(&token);
 
         index_t idx;
@@ -3375,7 +3375,7 @@ bool LoadObjWithCallback(std::istream &inStream, const callback_t &callback,
     if (token[0] == 'g' && IS_SPACE((token[1]))) {
       names.clear();
 
-      while (!IS_NEW_LINE(token[0])) {
+      while (!IS_NEW_LINE(token[0]) && token[0] != '#') {
         std::string str = parseString(&token);
         names.push_back(str);
         token += strspn(token, " \t\r");  // skip tag
